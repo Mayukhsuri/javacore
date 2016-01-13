6861c6442159e28ac3f975704ef6219b6c934cac
Mayukhsuri <mayukhsuri03@gmail.com>
2016-01-13 15:58:03 -0600

    checking formulae.
    checking formulae.

---
+++ src/com/digitek/mathoperations/Addition.java
@@ -0,0 +1,31 @@
+package com.digitek.mathoperations;
+
+public class Addition {
+	public static void addNumbers()  {
+	
+	int a = 15;
+	int b = 20;
+	
+	int c = a + b;
+	
+	System.out.println("Result of two numbers:" +c);
+	}
+	public static void subtractNumbers(){
+		int a = 15;
+		int b = 20;
+				int c = a-b;
+		System.out.println("Result of two numbers:" +c);
+		
+		}		
+		
+		
+public static void main(String args[]) {
+	addNumbers();
+	subtractNumbers();
+	
+	
+}
+
+	}
+	
+
